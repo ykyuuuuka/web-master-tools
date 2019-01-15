8353d2c2defd01d2e1ee765ba10d5b84c5020684
ykyuuuuka <ykyuuuuka@gmail.com>
2019-01-15 22:46:14 +0900

    add files.

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-	<title>CODE CRAWLER</title>
+	<title>web master tools</title>
+
 	<link rel="stylesheet" href="/app-resource/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
 	<link rel="stylesheet" href="/app-resource/css/style.css">
@@ -30,21 +31,18 @@
 					if(isset($_GET['panel'])) {
 						$panel_type = $_GET['panel'];
 						//SEOチェックアプリケーション
-						if($panel_type == '0') include('app-resource/include/seo_form.php');
-						if($panel_type == '0-1') include('app-resource/include/seo_issue.php');
-						if($panel_type == '0-2') include('app-resource/include/seo_issue_page.php');
+						// if($panel_type == '0') include('app-resource/include/seo_form.php');
+						// if($panel_type == '0-1') include('app-resource/include/seo_issue.php');
+						// if($panel_type == '0-2') include('app-resource/include/seo_issue_page.php');
 						//404チェックアプリケーション
-						if($panel_type == '2') include('app-resource/include/404_top.php');
+						// if($panel_type == '2') include('app-resource/include/404_top.php');
 						//サイトスクレイプスプリケーション
 						if($panel_type == '5') include('app-resource/include/scrape_form.php');
 						if($panel_type == '5-1') include('app-resource/include/scrape_check.php');
 						if($panel_type == '5-2') include('app-resource/include/scrape_result.php');
 						if($panel_type == '5-3') include('app-resource/include/scrape_csv.php');
-
-						//実験用のファイル（リリース禁止）
-						if($panel_type == '99') include('app-resource/include/mitsui.php');
 					} else {
-						header('Location: /?panel=0');
+						header('Location: /?panel=5');
 					}
 				?>
 
@@ -55,7 +53,6 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	<!-- <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=desert"></script> -->
 	<script src="/app-resource/js/app.js"></script>
 </body>
 </html>
