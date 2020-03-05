2cf91353dddcde7e3372ddc112820047789468cb
ykyuuuuka <ykyuuuuka@gmail.com>
2020-03-05 16:19:02 +0900

    説明を追加

--- app-resource/include/scrape_readme.php
+++ app-resource/include/scrape_readme.php
@@ -12,10 +12,17 @@
 	<li>php includeを許可</li>
 </ul>
 
+<h2 class="h4 mb-3">実行方法</h2>
+<ul class="mb-5">
+	<li>任意のディレクトリに解凍したファイルを XAMPP や MAMP などの Apache ディストリビューションソフトウェアで構築したローカル環境に可能します。（その際、「web-master-tools-master」がルートディレクトリとなるよう、httpd.conf または httpd-vhosts.conf を調整してください）</li>
+	<li>エントリーポイントはindex.phpですので例えば「localhost:8888/index.php」などでアプリケーションの実行が可能です。</li>
+</ul>
+<p class="normal">XAMPP のセットアップに不慣れな方は 「<a href="http://creator.aainc.co.jp/archives/6388" target="_blank">xamppでローカル作業環境構築をしよう！</a>」 などを参考ください。</p>
+
 <h2 class="h4 mb-3">リクエスト制限の解除</h2>
-<p class="normal">本アプリケーションはherokuでの実行を前提に、サーバーで30秒以上かかることが予想されるボリュームのリクエストはデフォルトで拒否していますが、 setting.php ファイルの以下の変数でリクエスト制限を解除できます。</p>
+<p class="normal">本アプリケーションはherokuでの実行を前提に、サーバーで30秒以上かかることが予想されるボリュームのリクエストはデフォルトで拒否していますが、「/app-resource/include/setting.php」の以下の変数を変更することでリクエスト制限を解除できます。</p>
 <div class="bg-code mb-5">
-	$request_limit = 0;　　//デフォルトの設定。リクエスト制限あり<br>
-	$request_limit = 1;　　//リクエスト制限なし
+	$request_limit = 0;　　//制限あり（デフォルト）<br>
+	$request_limit = 1;　　//制限なし
 </div>
 
--- app-resource/include/setting.php
+++ app-resource/include/setting.php
@@ -3,8 +3,8 @@
 	set_time_limit(0);					//処理時間の制限なし
 	ini_set('memory_limit', -1);		//メモリの制限なし
 
-	//制限解除オプション
-	$request_limit = 0;
+	//制限解除オプション　制限解除は「1」を代入
+	$request_limit = 1;
 ?>
 
 
